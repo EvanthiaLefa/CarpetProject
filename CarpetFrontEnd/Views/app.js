66345d48dcf855524f453ac752ebf99cca9e53de
Evanthia Lefa <74722922+EvanthiaLefa@users.noreply.github.com>
2023-05-14 11:41:09 +0300

    Update app.js

--- CarpetFrontEnd/Views/app.js
+++ CarpetFrontEnd/Views/app.js
@@ -1,3 +1,4 @@
+//uses the fetch API to retrieve data from a local server running at http://localhost:3004/users 
 const fetchData = async () => {
   try {
     const response = await fetch('http://localhost:3004/users');
@@ -7,29 +8,32 @@ const fetchData = async () => {
     console.log(err);
   }
 }
-const handleData = data => {
+//hat takes the retrieved JSON data as input and generates a Bootstrap card for each contact in the data.
+const handleData = jsonData => {
   const row = document.querySelector('.row');
-  data.forEach(user => {
+  jsonData.forEach(contact => {
     const card = document.createElement('div');
     card.className = 'col-sm-6 mt-2';
     card.innerHTML = `
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">${user.name}</h5>
-             <h6 class="card-title">${user.email}</h6>
+            <h5 class="card-title">${contact.name}</h5>
+             <h6 class="card-title">${contact.email}</h6>
             <a href="#" class="btn btn-primary float-end">View</a>
             <a href="#" class="btn btn-primary float-end">Delete</a>
           </div>
         </div>
       `;
     row.appendChild(card);
- });
+  });
 }
+//The fetchData function is invoked and the Promise it returns is chained with .then and .catch methods.
+// If the Promise resolves successfully, the handleData function is called otherwise the error is logged to the console.
 fetchData()
   .then(handleData)
   .catch(error => {
     console.error(error);
-});
+  });
 
 const formEl = document.querySelector('form');
 
