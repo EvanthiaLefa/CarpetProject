77a783219bbc367b9842d944793d2d9276ac88c5
Evanthia Lefa <74722922+EvanthiaLefa@users.noreply.github.com>
2023-05-14 20:10:23 +0300

    Update app2.js

--- CarpetFrontEnd/Views/app2.js
+++ CarpetFrontEnd/Views/app2.js
@@ -1,13 +1,18 @@
+/**
+ *  This code sets up an event listener on the .row element for a click event. When the deleteBtn is clicked within a card element,
+ *  the corresponding card is deleted from the DOM and from the server using a DELETE request.
+ */
+
 const row = document.querySelector('.row');
 row.addEventListener('click', (event) => {
-  if (event.target.id === 'deleteBtn') {
-    const card = event.target.closest('.card');
-    const id = card.id;
+  if (event.target.id === 'deleteBtn') { // checks if the clicked element has an id of deleteBtn.
+    const card = event.target.closest('.card'); // selects the closest ancestor element of the clicked element that has a class of card and assigns it to the card constant.
+    const id = card.id;    //This assigns the id attribute of the card to the id variable
     fetch(`http://localhost:3004/users/${id}`, {
-      method: 'DELETE'
+      method: 'DELETE'   // This makes a DELETE request to the server with the corresponding id`.
     })
     .then(response => {
-      if (response.ok) {
+      if (response.ok) { // Checks if the response status is OK (2xx).
         card.remove(); // Remove the card element from the DOM
       } else {
         throw new Error('Failed to delete user.');
