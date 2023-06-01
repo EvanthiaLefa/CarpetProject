3c1c6a361d3e3d6efaa08bf4b209b594510ee17d
Evanthia Lefa <74722922+EvanthiaLefa@users.noreply.github.com>
2023-06-01 20:56:39 +0300

    Update app.js

--- CarpetFrontEnd/Views/createForm/app.js
+++ CarpetFrontEnd/Views/createForm/app.js
@@ -2,7 +2,6 @@
 const form = document.querySelector('form');
 
 //Retrieves the values from form input fields / Uses the fetch API to send a POST request 
-//to local server running at http://localhost:3004/users, containing the values of the two input fields in JSON format. 
 function handleSubmit(event) {
   event.preventDefault();  //prevent the page from refresh.
   const firstName = form.querySelector('#floatingFirstName');
